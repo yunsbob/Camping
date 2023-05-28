d35dab5ffb4a5d244a5fdccc22584a36f75eb7de
YunsBob <w124564@naver.com>
2023-05-28 23:07:41 +0900

    init

---
+++ camping_back/src/main/java/com/ssafy/camping/member/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,48 @@
+package com.ssafy.camping.member.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.ssafy.camping.member.exception.UnAuthorizedException;
+import com.ssafy.camping.member.model.service.JwtTokenService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    private static final String HEADER_AUTH = "Authorization";
+
+    private final JwtTokenService jwtTokenService;
+
+    @Autowired
+    public LoginCheckInterceptor(JwtTokenService jwtTokenService){
+        this.jwtTokenService = jwtTokenService;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        try {
+            if (request.getMethod().equals("OPTIONS")) {
+                return true;
+            }
+
+            final String token = request.getHeader(HEADER_AUTH);
+
+            if (token != null && jwtTokenService.checkToken(token)) {
+                return true;
+            } else {
+                throw new UnAuthorizedException();
+            }
+        }
+        catch(IllegalArgumentException e){
+            throw new UnAuthorizedException();
+        }
+
+    }
+
+}
